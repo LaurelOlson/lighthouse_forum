f480a0480b1afde5f4ff0473f67b47b25b461baf
LaurelOlson <laurel.olson@outlook.com>
2016-03-01 00:11:28 +0000

    added edit post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id]) # set @post to the existing post to be updated - this means attributes will be preloaded in form_for
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
   protected
 
   def post_params
